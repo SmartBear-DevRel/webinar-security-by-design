a576695af7ce50e382d92b46d4ce7ac611271b48
Frank Kilcommins <frank.kilcommins@smartbear.com>
2023-10-20 14:19:11 +0100

    base setup set1

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# webinar-security-by-design
-A repo storing assets related to webinars demonstrating secure-by-design practices for APIs, and how security concerns can be applied across the API lifecycle.
+# API Security and 'Security by Design' practices
+
+Welcome 👋
+
+This repository stores assets related to webinars demonstrating secure-by-design practices for APIs, and how security concerns can be applied across the API lifecycle.
+
+The two-part webinar series are broken down as follows:
+ - Part I: covers the theoretical aspects of API security (covering the **what/why** of API security, the **2023 OWASP Top 10 Risks for APIs**, common **design vulnerability** pitfalls, and more..). Assets from the webinar are as follows:
+    - [YouTube Recording](https://www.youtube.com/watch?v=acXpD1tRmCQ)
+    - [Slides](https://assets.smartbear.com/transfer/388708a6e1980ed76ed7a194110d44f95c49790b294da03aa5906aea28806ce1)
+    - [2023 OWASP Top 10 Snippet Videos](https://www.youtube.com/playlist?list=PLrA5ciulugn8nydmfvt9cGBgDFqg8XbEt)
+ - Part II: covers a hands-on walkthrough how to apply some of the learnings from part I throughout the API lifecycle. The majority of assets in this repository relate to the hands-on part II.
+
+## Part II - Context and Instructions.....
---
+++ launchReadyAPITestRunner.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+## ACC
+# ENDPOINT=https://sbdevrel-fua-smartbearcoin-acc.azurewebsites.net/api/payees
+## PROD
+# ENDPOINT=https://sbdevrel-fua-smartbearcoin-prd.azurewebsites.net/api/payees
+
+ENDPOINT=${ENDPOINT:-'https://sbdevrel-fua-smartbearcoin-acc.azurewebsites.net/api'}
+PROJECT_FOLDER=${PROJECT_FOLDER:-'ReadyAPI_Tests'}
+PROJECT_FILE=${PROJECT_FILE:-'secure-by-design-readyapi-project.xml'}
+
+MISSING=()
+[ ! "$PROJECT_FOLDER" ] && MISSING+=("PROJECT_FOLDER")
+[ ! "$PROJECT_FILE" ] && MISSING+=("PROJECT_FILE")
+[ ! "$ENDPOINT" ] && MISSING+=("ENDPOINT")
+[ ! "$SLM_API_KEY" ] && MISSING+=("SLM_API_KEY")
+
+case $(uname -sm) in
+'Darwin x86' | 'Darwin x86_64' | 'Darwin arm64' | 'Windows')
+    ENDPOINT=${ENDPOINT/localhost/host.docker.internal}
+    ;;
+esac
+if [ ${#MISSING[@]} -gt 0 ]; then
+    echo "ERROR: The following environment variables are not set:"
+    printf '\t%s\n' "${MISSING[@]}"
+    exit 1
+fi
+
+echo "executing functional tests for ${PROJECT_FILE}"
+docker run --rm --network="host" \
+    -v=${PWD}/${PROJECT_FOLDER}:/project \
+    -e SLM_LICENSE_SERVER="https://api.slm.manage.smartbear.com:443" \
+    -e API_KEY=${SLM_API_KEY} \
+    -e ENDPOINT=${ENDPOINT} \
+    -e PROJECT_FILE=${PROJECT_FILE} \
+    -e COMMAND_LINE="'-e${ENDPOINT}' '-f/project' '-RJUnit-Style HTML Report' /project/${PROJECT_FILE}" \
+    smartbear/ready-api-soapui-testrunner:latest
+
+echo "executing security tests for ${PROJECT_FILE}"
+docker run --rm --network="host" \
+    -v=${PWD}/${PROJECT_FOLDER}:/project \
+    -e SLM_LICENSE_SERVER="https://api.slm.manage.smartbear.com:443" \
+    -e API_KEY=${SLM_API_KEY} \
+    -e ENDPOINT=${ENDPOINT} \
+    -e PROJECT_FILE=${PROJECT_FILE} \
+    -e COMMAND_LINE="'-e${ENDPOINT}' '-f/project' '-RJUnit-Style HTML Report' /project/${PROJECT_FILE}" \
+    smartbear/readyapi-security-testrunner:latest
