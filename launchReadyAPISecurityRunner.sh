9bf21760ca2b53e7b1f588c2b965cdacd0f8747b
Frank Kilcommins <frank.kilcommins@smartbear.com>
2023-10-24 21:54:01 +0100

    separate functional and security test

--- launchReadyAPISecurityRunner.sh
+++ launchReadyAPISecurityRunner.sh
@@ -26,12 +26,12 @@ if [ ${#MISSING[@]} -gt 0 ]; then
     exit 1
 fi
 
-echo "executing functional tests for ${PROJECT_FILE}"
+echo "executing security tests for ${PROJECT_FILE}"
 docker run --rm --network="host" \
     -v=${PWD}/${PROJECT_FOLDER}:/project \
     -e SLM_LICENSE_SERVER="https://api.slm.manage.smartbear.com:443" \
     -e API_KEY=${SLM_API_KEY} \
     -e ENDPOINT=${ENDPOINT} \
     -e PROJECT_FILE=${PROJECT_FILE} \
-    -e COMMAND_LINE="'-e${ENDPOINT}' '-f/project' '-RJUnit-Style HTML Report' /project/${PROJECT_FILE}" \
-    smartbear/ready-api-soapui-testrunner:latest
+    -e COMMAND_LINE="'-f/project' '-RSecurity Issues Report' -FPDF '-Elocal' /project/${PROJECT_FILE}" \
+    smartbear/readyapi-security-testrunner:latest
